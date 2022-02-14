6dd88364eb4a62f799db5b6258db38c7f4680aa1
Yohan Youmbi <31018343+iEatFishFingers@users.noreply.github.com>
2022-02-14 15:26:02 +0000

    Login (No design)

---
+++ Coursework/App.js
@@ -0,0 +1,22 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+import HomeScreen from './screens/HomeScreen';
+import Login from './screens/Login';
+
+export default function App() {
+  const Stack = createNativeStackNavigator();
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{headerShown: false}}>
+        <Stack.Screen name='Login' component={Login}/>
+        <Stack.Screen name='Home' component={HomeScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+
