c3cbb2e67bed3cf8bb552d04aeaecd59bee1d6fc
iEatFishFingers <31018343+iEatFishFingers@users.noreply.github.com>
2022-03-05 07:34:22 +0000

    rearrange

---
+++ SpaceBookApp/screens/bottomTab.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import Feed from './SocialParts/Feed'
+import Profile from './SocialParts/Profile';
+import Friends from './SocialParts/MeetFriends';
+import EditProfile from './SocialParts/EditProfile';
+import Cam from './SocialParts/Cam';
+
+
+const Stackv2 = createBottomTabNavigator();
+
+class bottomTab extends Component{
+  render(){
+    return(
+      <Stackv2.Navigator screenOptions={{headerShown: false}}>
+        <Stackv2.Screen name="Feed" component={Feed}/>
+        <Stackv2.Screen name="Profile" component={Profile}/>
+        <Stackv2.Screen name="Friends" component={Friends}/>
+        <Stackv2.Screen name="EditProfile" component={EditProfile}/>
+        <Stackv2.Screen name="Cam" component={Cam}/>
+      </Stackv2.Navigator>
+    );
+  }
+}
+
+
+
+export default bottomTab;
